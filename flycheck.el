229dcc2a91327e4df5427d983d3315f0cf0800fc
yuhe <gfgkmn@gmail.com>
2025-01-23 15:53:14 +0800

    ruff use no color

--- flycheck.el
+++ flycheck.el
@@ -10675,7 +10675,9 @@ which should be used and reported to the user."
 To override the path to the ruff executable, set
 `flycheck-python-ruff-executable'.
 See URL `http://pypi.python.org/pypi/ruff'."
-  :command ("python3"
+  :command ("env"
+            "NO_COLOR=1"
+            "python3"
             "-m"
             "ruff"
             "check"
